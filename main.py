8eb8e0e140544cca58b961017656597fd8de0438
Julius Markwei <juliusmarkwei2000@gmail.com>
2023-02-19 03:06:14 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from pandas import DataFrame
+from pandas import read_csv, read_json, read_xml
+from glob import glob
+
+
+def extract_from_csv(filename):
+    dataframe = read_csv(filename)
+    return dataframe
+
+def extract_from_json(filename):
+    dataframe = read_json(filename)
+    return dataframe
+
+def extract_from_xml(filename):
+    dataframe = read_xml(filename)
+    return dataframe
+
+def extract_data():
+    # create an empty data frame to hold expected data
+    extracted_data = DataFrame(columns=[1, 2, 3, 4, 5])
+
+    # process all csv files
+    for csvfile in glob("*.csv"):
+        extracted_data.append(extract_from_csv(csvfile), ignore_index = True)
+    
+    # rpocess all json files
+    for jsonfile in glob("*.json"):
+        extracted_data.append(extract_from_json(jsonfile), ignore_index = True)
+
+    for xmlfile in glob("*.xml"):
+        extracted_data.append(extract_from_xml(xmlfile), ignore_index = True)
+
+    return extracted_data
